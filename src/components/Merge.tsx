098841c9d0bb990e1f8c720801d6a0df0c259537
Crptk <amiriedrees2005@gmail.com>
2026-04-21 17:28:15 -0400

    Feature:  Finished merge section

--- src/components/Merge.tsx
+++ src/components/Merge.tsx
@@ -1,18 +1,23 @@
+import { useFadeIn } from "../hooks/useFadeIn";
+
 export default function Merge() {
+    const ref = useFadeIn<HTMLDivElement>();
     return (
-        <div className="merge-info-section">
-            <h1>
-                Merge Scenes, Stay in Control.
-            </h1>
-            <p>
-                No scene detection system is perfect, and AMVerge is built with that in mind. 
-                Fast motion, flashes, camera shakes, or intense fight sequences can sometimes create 
-                extra cuts. Instead of forcing you to manually rebuild clips in an editor, AMVerge lets you 
-                instantly merge selected scenes back together into one seamless segment.
-                This also keeps your project panel clean and organized. Rather than dealing with dozens of tiny clips, 
-                you can combine related moments into larger, easy-to-manage scenes that are faster to browse, 
-                preview, and import into your editing workflow.
-            </p>
+        <div className="merge-info-section fade-in" ref={ref}>
+            <div className="merge-info-text">
+                <h1>
+                    <span>Merge</span> Scenes, Stay in Control.
+                </h1>
+                <p>
+                    No scene detection system is perfect, and AMVerge is built with that in mind. 
+                    Fast motion, flashes, camera shakes, or intense fight sequences can sometimes create 
+                    extra cuts. Instead of forcing you to manually rebuild clips in an editor, AMVerge lets you 
+                    instantly merge selected scenes back together into one seamless segment.
+                    This also keeps your project panel clean and organized. Rather than dealing with dozens of tiny clips, 
+                    you can combine related moments into larger, easy-to-manage scenes that are faster to browse, 
+                    preview, and import into your editing workflow.
+                </p>
+            </div>
         </div>
     )
 }
